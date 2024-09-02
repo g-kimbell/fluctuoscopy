b70c2e5881e0a7f807809fed5ef6b75d7f8e9b21
g-kimbell <grahamkimbell@outlook.com>
2024-09-02 17:05:30 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fluctuoscopy',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'multiprocess',
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+        ],
+    },
+    package_data={
+        'fscope': ['bin/*'],
+    },
+    author='Graham Kimbell',
+    description='A Python wrapper with additional helper functions for the FSCOPE program, written by Andreas Glatz',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/g-kimbell/fluctuoscopy',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Windows',
+    ],
+    python_requires='>=3.6',
+)
