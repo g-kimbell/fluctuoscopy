8344bd6d33bd486004aa2628a83eb2078d00fedc
g-kimbell <grahamkimbell@outlook.com>
2024-09-03 17:05:01 +0200

    Expose fscope function

--- fluctuoscopy/__init__.py
+++ fluctuoscopy/__init__.py
@@ -1,7 +1,8 @@
-from .fluctuosco import fscope_full_func, fscope_delta, fscope_delta_wrapped, weak_localization, weak_antilocalization, calc_tau, AL2D
+from .fluctuosco import *
 
 __all__ = [
     'fscope_full_func',
+    'fscope',
     'fscope_delta',
     'fscope_delta_wrapped',
     'weak_localization',
